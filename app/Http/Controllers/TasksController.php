f27d0e4cf390c702b808ba1c20c520c04c8786e2
naomi.M <naomi-maruya@dac.co.jp>
2020-06-05 11:09:05 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -30,6 +30,10 @@ class TasksController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            'content' => 'required|max:255',
+        ]);
+        
         $task = new Task;
         $task->content = $request->content;
         $task->save();
@@ -60,6 +64,10 @@ class TasksController extends Controller
 
     public function update(Request $request, $id)
     {
+         $request->validate([
+            'content' => 'required|max:255',
+        ]);
+
         $task = Task::findOrFail($id);
         // メッセージを更新
         $task->content = $request->content;
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,9 +8,9 @@
     </head>
 
     <body>
-        
         @include('commons.navbar')
         <div class="container">
+            @include('commons.error_messages')
             @yield('content')
         </div>
 
